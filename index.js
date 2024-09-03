95022c9498ecdcd3dfb970bb013598e33d730fac
Jbasilio55 <j.l.basilio55@gmail.com>
2024-09-03 15:19:12 -0400

    added copy clipboard

--- index.js
+++ index.js
@@ -25,4 +25,30 @@ function generatePassword() {
   password2.value = text2;
 }
 
+function copyPassword1() {
+  let password1 = document.getElementById("text1");
+  password1.select();
+  navigator.clipboard.writeText(password1.value);
+
+  if (password1.value.length < 1) {
+    alert("Please enter a password length and generate a password.");
+  } else {
+    alert("Copied the text: " + password1.value);
+  }
+}
+
+function copyPassword2() {
+  let password2 = document.getElementById("text2");
+  password2.select();
+  navigator.clipboard.writeText(password2.value);
+
+  if (password2.value.length < 1) {
+    alert("Please enter a password length and generate a password.");
+  } else {
+    alert("Copied the text: " + password2.value);
+  }
+}
+
 window.generatePassword = generatePassword;
+window.copyPassword1 = copyPassword1;
+window.copyPassword2 = copyPassword2;
