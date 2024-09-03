0879792d738152115191df0942734b06bba37256
Jbasilio55 <j.l.basilio55@gmail.com>
2024-09-03 14:42:11 -0400

    comleted password length

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ let chArray = characters;
 console.log(chArray[2]);
 
 function generatePassword() {
-  let text1 = "";
-  let text2 = "";
-  let textLength = 15;
-
   let password1 = document.getElementById("text1");
   let password2 = document.getElementById("text2");
+  let textLength = document.getElementById("chLength").value;
+
+  if (textLength > 21) {
+    textLength = 20;
+  }
+
+  let text1 = "";
+  let text2 = "";
 
   for (let i = 0; i < textLength; i++) {
     text1 += chArray[Math.floor(Math.random() * chArray.length)];
